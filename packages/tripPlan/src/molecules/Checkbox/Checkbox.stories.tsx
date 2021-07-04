de627e5f6dc1c8518a45eb55956a4d7b15006e82
ianwei <ikaeuu@gmail.com>
2021-07-04 13:43:59 +1000

    feat: chromatic
    publish storybook to chromatic for sharing design
    
    resolve #013

--- packages/tripPlan/src/molecules/Checkbox/Checkbox.stories.tsx
+++ packages/tripPlan/src/molecules/Checkbox/Checkbox.stories.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { Checkbox } from './Checkbox';
 import { CheckboxProps } from './Checkbox.props';
 import { Story } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
 
 export default {
     title: 'Molecules/Checkbox',
     component: Checkbox
 };
 
-const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
+const Template: Story<CheckboxProps> = (args) => (
+    <Checkbox {...args} label={text('Label', args.label)} />
+);
 
-export const Primary = Template.bind({});
+export const Primary: Story<CheckboxProps> = Template.bind({});
 
 Primary.args = {
     label: 'Testing',
     handleCheckboxChange(label: string) {
         console.log(label);
     }
-} as CheckboxProps;
+};
